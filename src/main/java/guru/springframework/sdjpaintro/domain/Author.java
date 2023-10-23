2997b397d98955df35c0028cce38bae61ce1eefe
ddmitry <dvdmit@gmail.com>
2023-10-23 16:29:48 +0300

    Module 6 end

--- src/main/java/guru/springframework/sdjpaintro/domain/Author.java
+++ src/main/java/guru/springframework/sdjpaintro/domain/Author.java
@@ -6,31 +6,19 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
-/**
- * Created by jt on 6/12/21.
- */
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Book {
+public class Author {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @EqualsAndHashCode.Include
     private Long id;
 
-    private String title;
-    private String isbn;
-    private String publisher;
-    private Long authorId;
-
-    public Book(String title, String isbn, String publisher, Long authorId) {
-        this.title = title;
-        this.isbn = isbn;
-        this.publisher = publisher;
-        this.authorId = authorId;
-    }
+    private String firstName;
+    private String lastName;
 }
